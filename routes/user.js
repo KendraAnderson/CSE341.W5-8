4f9e27e6f2a411f1fc10b93442c6d2d336e2839e
KendraAnderson <kor04003@byui.edu>
2023-03-09 17:04:15 -0800

    updated json with /users url

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const routes = require('express').Router();
 const userController = require('../controllers/user');
 
-routes.get('/', userController.getAll);
+routes.get('/users', userController.getAll);
 
 routes.get('/users/:username', userController.getUser);
 
-routes.post('/', userController.create);
+routes.post('/users', userController.create);
 
 module.exports = routes;
