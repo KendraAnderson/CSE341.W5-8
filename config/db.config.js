d938c4f91207746a393789eecd8c068c9426a5fb
KendraAnderson <kor04003@byui.edu>
2023-03-10 17:52:59 -0800

    url fix

--- config/db.config.js
+++ config/db.config.js
@@ -2,5 +2,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  url: process.env.MONGODB_URI
+  url: process.env.MONGODB_URI || 'mongodb+srv://kortask:qwer1234@cluster0.uadi7m1.mongodb.net/Recipes'
 };
