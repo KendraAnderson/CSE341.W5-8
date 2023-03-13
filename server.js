7a66fa203a19c10828b8bb8b20d1bd4f1d31346f
KendraAnderson <kor04003@byui.edu>
2023-03-13 11:47:57 -0700

    auth0 base partial

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
+//OAuth code
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+/*
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
@@ -30,4 +52,11 @@ db.mongoose
   .catch((err) => {
     console.log('Cannot connect to the database!', err);
     process.exit();
+  });*/
+  app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
   });
