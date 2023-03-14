e785a1f90912e60795ce35f9aa750b329a6b3f81
KendraAnderson <kor04003@byui.edu>
2023-03-14 13:56:32 -0700

    requireAuth in swagger defined

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('../swagger.json');
+const { requiresAuth } = require('express-openid-connect');
 
 routes.use('/api-docs', requiresAuth(), swaggerUi.serve);
 routes.get('/api-docs', requiresAuth(), swaggerUi.setup(swaggerDoc));
