f759415720c55f97acb0d96e6dd78961dc8330b9
KendraAnderson <kor04003@byui.edu>
2023-03-14 13:50:10 -0700

    auth required for api-docs

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,7 +2,7 @@ const routes = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('../swagger.json');
 
-routes.use('/api-docs', swaggerUi.serve);
-routes.get('/api-docs', swaggerUi.setup(swaggerDoc));
+routes.use('/api-docs', requiresAuth(), swaggerUi.serve);
+routes.get('/api-docs', requiresAuth(), swaggerUi.setup(swaggerDoc));
 
 module.exports = routes;
